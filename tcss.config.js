786cec51ab7cce70f52a9cc0d904a8d3dbaf427c
Contak-cpu <erickkarcher22@gmail.com>
2025-09-09 17:57:13 -0300

    Actualización del proyecto Konrad - formularios de propiedades y mejoras en componentes

---
+++ tcss.config.js
@@ -0,0 +1,3 @@
+[33m725d178[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m feat: Mejoras de responsividad móvil, favoritos y filtros personalizables
+[33m68c1f30[m Fix: Resolver conflicto de merge en constants.ts y migrar a estructura modular
+[33m9271946[m Commit de cambios actuales

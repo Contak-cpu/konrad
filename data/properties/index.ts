12d9bc338a06b6ceddc6805758f57987e7d0578c
Contak-cpu <erickkarcher22@gmail.com>
2025-09-16 11:40:22 -0300

    Actualizar logo y ocultar propiedades temporalmente
    - Actualizar logo con nueva imagen de Discord optimizada
    - Comentar temporalmente propiedades no deseadas
    - Mostrar solo Beruti 361, Alberdi y Edison, y Giachino 2455
    - Optimizar carga del sitio con catálogo enfocado

--- data/properties/index.ts
+++ data/properties/index.ts
@@ -1,33 +1,33 @@
 import { Property } from '../../types';
 import { giachino2455 } from './giachino2455';
-import { ferrando841 } from './ferrando841';
+// import { ferrando841 } from './ferrando841';
 import { alberdiEdison } from './alberdiEdison';
-import { hilarioLagos } from './hilarioLagos';
-import { urquiza } from './urquiza';
-import { saluzzi } from './saluzzi';
-import { giachino } from './giachino';
-import { gavazza } from './gavazza';
-import { peronCastex } from './peronCastex';
-import { sanRoque } from './sanRoque';
-import { victoria547 } from './victoria547';
-import { pestalozziLote } from './pestalozziLote';
-import { venezuela1384 } from './venezuela1384';
+// import { hilarioLagos } from './hilarioLagos';
+// import { urquiza } from './urquiza';
+// import { saluzzi } from './saluzzi';
+// import { giachino } from './giachino';
+// import { gavazza } from './gavazza';
+// import { peronCastex } from './peronCastex';
+// import { sanRoque } from './sanRoque';
+// import { victoria547 } from './victoria547';
+// import { pestalozziLote } from './pestalozziLote';
+// import { venezuela1384 } from './venezuela1384';
 import { berutti361 } from './berutti361';
 
 export const properties: Property[] = [
   giachino2455,
-  ferrando841,
+  // ferrando841,
   alberdiEdison,
-  hilarioLagos,
-  urquiza,
-  saluzzi,
-  giachino,
-  victoria547,
-  pestalozziLote,
-  gavazza,
-  peronCastex,
-  sanRoque,
-  venezuela1384,
+  // hilarioLagos,
+  // urquiza,
+  // saluzzi,
+  // giachino,
+  // victoria547,
+  // pestalozziLote,
+  // gavazza,
+  // peronCastex,
+  // sanRoque,
+  // venezuela1384,
   berutti361,
 ];
 
